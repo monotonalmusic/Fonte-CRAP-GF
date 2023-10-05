c87a0799b4ae8d122d067a37720753fe5dddc36d
Gabriel <gehughes28@gmail.com>
2023-10-05 09:19:00 +0200

    hover + active on hamburger menu

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ for (let i= 0; i < items.length; i++) {
         this.className +=" active";
     });
 }
+let burgerContainer = document.getElementById('menu');
+
+let burgerItems = menuContainer.getElementsByClassName("burger-item");
+
+let burgerLength = burgerItems.length;
+
+for (let i= 0; i < burgerItems.length; i++) {
+    burgerItems[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className +=" active";
+    });
+}
+
+
